2e37c4ca19eef623330e77c694c05736035d12aa
searhcaz0099 <searchaz0099@gmail.com>
2023-07-22 20:18:20 +0700

    .

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,29 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
   },
 ];
 
